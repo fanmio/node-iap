0e49ecb633105c560cae7728c247c2e3805789c9
Marcin Butanowicz <butan@marcin.local>
2024-04-26 14:20:50 +0200

    Added vizio support

--- index.js
+++ index.js
@@ -1,85 +1,91 @@
-'use strict';
+"use strict";
 
 const platforms = {
-	amazon: require('./lib/amazon'),
-	apple: require('./lib/apple'),
-	google: require('./lib/google'),
-	roku: require('./lib/roku')
+  amazon: require("./lib/amazon"),
+  apple: require("./lib/apple"),
+  google: require("./lib/google"),
+  roku: require("./lib/roku"),
+  vizio: require("./lib/vizio"),
 };
 
 const promisify = (fn) => {
-	return (...args) => {
-		return new Promise((resolve, reject) => {
-			fn(...args, (err, res) => {
-				return (err ? reject(err) : resolve(res));
-			});
-		});
-	};
+  return (...args) => {
+    return new Promise((resolve, reject) => {
+      fn(...args, (err, res) => {
+        return err ? reject(err) : resolve(res);
+      });
+    });
+  };
 };
 
 function verifyPayment(platform, payment, cb) {
-	function syncError(error) {
-		process.nextTick(function () {
-			cb(error);
-		});
-	}
+  function syncError(error) {
+    process.nextTick(function () {
+      cb(error);
+    });
+  }
 
-	if (!payment) {
-		return syncError(new Error('No payment given'));
-	}
+  if (!payment) {
+    return syncError(new Error("No payment given"));
+  }
 
-	const engine = platforms[platform];
+  const engine = platforms[platform];
 
-	if (!engine) {
-		return syncError(new Error(`Platform ${platform} not recognized`));
-	}
+  if (!engine) {
+    return syncError(new Error(`Platform ${platform} not recognized`));
+  }
 
-	engine.verifyPayment(payment, function (error, result) {
-		if (error) {
-			return cb(error);
-		}
+  engine.verifyPayment(payment, function (error, result) {
+    if (error) {
+      return cb(error);
+    }
 
-		result.platform = platform;
+    result.platform = platform;
 
-		cb(null, result);
-	});
+    cb(null, result);
+  });
 }
 
 function cancelSubscription(platform, payment, cb) {
-	function syncError(error) {
-		process.nextTick(function () {
-			cb(error);
-		});
-	}
+  function syncError(error) {
+    process.nextTick(function () {
+      cb(error);
+    });
+  }
 
-	if (!payment) {
-		return syncError(new Error('No payment given'));
-	}
+  if (!payment) {
+    return syncError(new Error("No payment given"));
+  }
 
-	const engine = platforms[platform];
+  const engine = platforms[platform];
 
-	if (!engine) {
-		return syncError(new Error(`Platform ${platform} not recognized`));
-	}
+  if (!engine) {
+    return syncError(new Error(`Platform ${platform} not recognized`));
+  }
 
-	if (!engine.cancelSubscription) {
-		return syncError(new Error(`Platform ${platform
-		} does not have cancelSubscription method`));
-	}
+  if (!engine.cancelSubscription) {
+    return syncError(
+      new Error(`Platform ${platform} does not have cancelSubscription method`)
+    );
+  }
 
-	engine.cancelSubscription(payment, function (error, result) {
-		if (error) {
-			return cb(error);
-		}
+  engine.cancelSubscription(payment, function (error, result) {
+    if (error) {
+      return cb(error);
+    }
 
-		cb(null, result);
-	});
+    cb(null, result);
+  });
 }
 
 exports.verifyPayment = (platform, payment, cb) => {
-	return (cb ? verifyPayment(platform, payment, cb) : promisify(verifyPayment)(platform, payment));
+  return cb
+    ? verifyPayment(platform, payment, cb)
+    : promisify(verifyPayment)(platform, payment);
 };
 
 exports.cancelSubscription = (platform, payment, cb) => {
-	return (cb ? cancelSubscription(platform, payment, cb) : promisify(cancelSubscription)(platform, payment));
+  return cb
+    ? cancelSubscription(platform, payment, cb)
+    : promisify(cancelSubscription)(platform, payment);
 };
